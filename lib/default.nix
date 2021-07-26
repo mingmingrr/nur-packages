1058fd98ae632b25ddfea69a72a2b38ad8386733
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-07-26 03:46:51 -0400

    lib.interpolate: init

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,16 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> { } }:
 
-with pkgs.lib; {
-  # Add your library functions here
-  #
-  # hexint = x: hexvals.${toLower x};
+with builtins;
+with pkgs.lib;
+
+rec {
+  evaluate = text: import (pkgs.writeText "evaluate.nix" text);
+  interpolate = regex: group: scope: text: let
+    process = item: if typeOf item == "string"
+    then item
+    else toString (evaluate
+      ( "{ "  + concatStringsSep "\n, " (attrNames scope)
+      + "\n}:\n" + elemAt item group) scope);
+  in concatMapStrings process (split regex text);
+  interpolateNix = interpolate "\\\$\\{(( }|[^\\}])*)}" 0;
 }
