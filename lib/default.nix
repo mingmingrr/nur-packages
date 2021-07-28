d4be94b314dd1287aa41b43d1f23cb4f4aef4e27
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-07-28 01:02:02 -0400

    lib: add attrs functions

--- lib/default.nix
+++ lib/default.nix
@@ -3,14 +3,25 @@
 with builtins;
 with pkgs.lib;
 
-rec {
-  evaluate = text: import (pkgs.writeText "evaluate.nix" text);
+let lib = rec {
+  evaluate = text: import (toFile "evaluate.nix" text);
+
   interpolate = regex: group: scope: text: let
+    scope' = concatStringsSep "\n, " (attrNames scope);
     process = item: if typeOf item == "string"
-    then item
-    else toString (evaluate
-      ( "{ "  + concatStringsSep "\n, " (attrNames scope)
-      + "\n}:\n" + elemAt item group) scope);
-  in concatMapStrings process (split regex text);
+      then item
+      else toString (evaluate
+         "{ ${scope'}\n}:\n${elemAt item group}" scope);
+    in concatMapStrings process (split regex text);
+
   interpolateNix = interpolate "\\\$\\{(( }|[^\\}])*)}" 0;
-}
+
+  attrsToList = attrs: map
+    (name: { name = name; value = getAttr name attrs; }) (attrNames attrs);
+
+  mapAttrsName = func: mapAttrsNameValue
+    ({name, value}: { name = func name value; value = value; });
+
+  mapAttrsNameValue = func: attrs: listToAttrs (map
+    ({name, value}: func name value) (attrsToList attrs));
+}; in lib
