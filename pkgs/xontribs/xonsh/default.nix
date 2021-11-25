b6058c795b44b0bb47c293bbadc405e12de454da
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2021-11-25 13:17:19 -0500

    xontribs: fix requirements

--- pkgs/xontribs/xonsh/default.nix
+++ pkgs/xontribs/xonsh/default.nix
@@ -1,16 +1,48 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi
+, python
+, enableFull ? true
+, distro
+, prompt-toolkit
+, pygments
+, pyperclip
+}:
+
 buildPythonPackage rec {
   pname = "xonsh";
   version = "0.11.0";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "0d9c3d9a4e8b8199ae697fbc9d1e0ae55085cdbdd4306d04813350996f9c15dc";
   };
-  propagatedBuildInputs = [ ];
+
+  propagatedBuildInputs = lib.optionals enableFull
+    [ distro prompt-toolkit pygments pyperclip ];
+
+  postPatch = ''
+    find scripts -name 'xonsh*' -exec sed -i -e "s|env -S|env|" {} \;
+    patchShebangs .
+    substituteInPlace scripts/xon.sh \
+      --replace 'python' "${python}/bin/python"
+  '';
+
+  makeWrapperArgs = [
+    "--prefix PYTHONPATH : ${placeholder "out"}/lib/${python.libPrefix}/site-packages"
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/xon.sh $makeWrapperArgs
+  '';
+
+  passthru = {
+    shellPath = "/bin/xonsh";
+  };
+
   doCheck = false;
+
   meta = with lib; {
     description = "Python-powered, cross-platform, Unix-gazing shell";
     homepage = "https://xon.sh";
-    # license = licenses.BSD;
+    license = licenses.bsd3;
   };
 }
--- pkgs/xontribs/xontrib-ergopack/default.nix
+++ pkgs/xontribs/xontrib-ergopack/default.nix
@@ -1,4 +1,22 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi
+, xontrib-argcomplete
+, xontrib-back2dir
+, xontrib-output-search
+, xontrib-pipeliner
+, xontrib-sh
+, xonsh-autoxsh
+, xontrib-history-encrypt
+, enableDev ? true
+, xontrib-hist-format        # extra == 'dev'
+, xontrib-readable-traceback # extra == 'dev'
+, hunter                     # extra == 'dev'
+, enableOnepath ? true
+, xontrib-onepath            # extra == 'onepath'
+, enablePrompt ? true
+, xontrib-prompt-bar         # extra == 'prompt'
+, xontrib-prompt-starship    # extra == 'prompt'
+}:
+
 buildPythonPackage rec {
   pname = "xontrib-ergopack";
   version = "0.1.4";
@@ -6,11 +24,28 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "003aba517e04622178ce572ef09e47f2ba38434b0c581ad4f6a315713bd19fbe";
   };
-  propagatedBuildInputs = [ ];
+  propagatedBuildInputs = [
+    xontrib-argcomplete
+    xontrib-back2dir
+    xontrib-output-search
+    xontrib-pipeliner
+    xontrib-sh
+    xonsh-autoxsh
+    xontrib-history-encrypt
+  ] ++ lib.optionals enableDev [
+    xontrib-hist-format
+    xontrib-readable-traceback
+    hunter
+  ] ++ lib.optionals enableOnepath [
+    xontrib-onepath
+  ] ++ lib.optionals enablePrompt [
+    xontrib-prompt-bar
+    xontrib-prompt-starship
+  ];
   doCheck = false;
   meta = with lib; {
     description = "Meta package that installs group of ergonomic xontribs in xonsh shell.";
     homepage = "https://github.com/anki-code/xontrib-ergopack";
-    # license = licenses.MIT;
+    license = licenses.mit;
   };
 }
